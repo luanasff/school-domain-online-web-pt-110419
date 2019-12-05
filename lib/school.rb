4a0cd0c56dd6febf94bda43775bec0d2877fe83d
Luana Faria Lane <luanasfff@gmail.com>
2019-12-05 17:20:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School 
+  
+  attr_reader :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name=school_name
+    @roster = {}
+  end 
+  
+  def add_student(grade,name) 
+    
+    @roster[grade] << name
+    @roster[grade] ||= []
+  end 
+  
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+end 
